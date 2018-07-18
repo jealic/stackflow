931a19307a0d780d0e3528a3e35f65625fae1863
Chiehyun <jealic@gmail.com>
2018-07-19 02:00:56 +0800

    build_association

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,5 @@
 class Answer < ApplicationRecord
+  belongs_to :user, counter_cache: true
+  belongs_to :question, counter_cache: true
+  has_many :upvotes
 end
--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,2 +1,5 @@
 class Upvote < ApplicationRecord
+  belongs_to :answer, counter_cache: true
+  belongs_to :user
+  belongs_to :question, counter_cache: true
 end
