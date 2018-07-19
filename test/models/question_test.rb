1b0e4c19d26699c2f3dd6c7a40bd05bc82a8fcb2
beranemo <beranemo@gmail.com>
2018-07-19 19:58:03 +0800

    geneate annotate

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id              :integer          not null, primary key
+#  topic           :string
+#  content         :text
+#  user_id         :integer
+#  answers_count   :integer          default(0)
+#  upvotes_count   :integer          default(0)
+#  favorites_count :integer          default(0)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
