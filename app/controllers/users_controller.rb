0979280b82a9077c260723a76cd08185456204f4
Chiehyun <jealic@gmail.com>
2018-07-21 00:49:44 +0800

    added users' templates: edit/index/show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :image)
+  end
+
+end
