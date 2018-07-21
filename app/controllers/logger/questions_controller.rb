adeaad732c2e05908755cb6d8cd5bcc627341886
xs1523c <xs1523c@gmail.com>
2018-07-21 14:19:23 +0800

    set routes & controller for logger_question

--- app/controllers/logger/questions_controller.rb
+++ app/controllers/logger/questions_controller.rb
@@ -1,5 +1,23 @@
 class Logger::QuestionsController < Logger::BaseController
   def index
-    
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+    @question.user = current_user
+    if @question.save
+      flash[:notice] = "question was successfully created"
+      redirect_to logger_questions_path
+    else
+      flash[:alert] = "question was failed to created"
+      render :index
+    end
+  end
+
+
+  private
+  def question_params
+    params.require(:question).permit(:title,:content)
   end
 end
