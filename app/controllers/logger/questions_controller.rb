0233b7e87d064100e91e44ed5e510969ff553935
xs1523c <xs1523c@gmail.com>
2018-07-21 16:26:35 +0800

    build logger/show.page

--- app/controllers/logger/questions_controller.rb
+++ app/controllers/logger/questions_controller.rb
@@ -16,6 +16,10 @@ class Logger::QuestionsController < Logger::BaseController
     end
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
 
   private
   def question_params
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,5 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.order(created_at: :desc)
   end
+  
 end
