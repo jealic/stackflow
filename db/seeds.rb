17febf138c7726f2b2adfac1daa3e6d16fff7ca2
Ling Chieh Yun <jealic@hotmail.com>
2018-07-27 22:50:41 +0800

    established seed for hashtags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hashtag.destroy_all
+
+hashtag_list = [
+  { name: "Ruby"},
+  { name: "PHP"},
+  { name: "CSS"},
+  { name: "Javascript"},
+  { name: "Java"},
+  { name: "C++"}
+]
+
+hashtag_list.each do |hashtag|
+  Hashtag.create(name: hashtag[:name])
+  puts "Hashtag #{hashtag[:name]} was generated."
+end
