a91cbcf165c30c276326edb684e25efd74acbc06
Ling Chieh Yun <Chiehyun@Lingde-MacBook-Air.local>
2018-07-27 22:34:17 +0800

    set up relationships again (q_tagships)

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,9 @@
 class Hashtag < ApplicationRecord
+  #table name: hashtags
+  #
+  # name     :string, null:false
+
+  
   has_many :questions, through: :q_tagships, dependent: :restrict_with_error
   has_many :q_tagships
 end
--- app/models/q_tagship.rb
+++ app/models/q_tagship.rb
@@ -1,2 +1,12 @@
 class QTagship < ApplicationRecord
+
+  #== schema Information
+
+  # Table Name: q_tagships
+  #
+  # question_id     :integer
+  # hashtag_id      :integer
+
+  belongs_to :hashtag
+  belongs_to :question
 end
