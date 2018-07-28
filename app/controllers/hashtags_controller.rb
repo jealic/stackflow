b51308d22815c8b8240f8bb307fee77b808ad84a
Ling Chieh Yun <jealic@hotmail.com>
2018-07-28 18:07:37 +0800

    able to add new hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -2,4 +2,20 @@ class HashtagsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :edit, :destory]
 
   
+  def create
+    @hashtag = Hashtag.new(hashtag_params)
+    if @hashtag.save
+      flash[:notice] = "Just added a new tag."
+    else
+      @hashtags = Hashtag.all
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def hashtag_params
+    params.require(:hashtag).permit(:name)
+  end
+  
 end
