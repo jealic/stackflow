c9a376c300cd2e7e7519d07543f40b66465d60d7
xs1523c <xs1523c@gmail.com>
2018-07-28 21:37:23 +0800

    hashtag add words at most 10 words

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,6 +4,7 @@ class Hashtag < ApplicationRecord
   # name     :string, null:false
 
   validates_presence_of :name
+  validates :name, length: { maximum: 10, message: "hashtag can be allowed at most 10 words", }
   
 
   has_many :q_tagships
