5a0f04305c1ec4a9263c8e935d5e9fa621cc823a
xs1523c <xs1523c@gmail.com>
2018-07-28 22:19:24 +0800

    hashtag cann only type eng or chinese && not allowed special char

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,8 +4,10 @@ class Hashtag < ApplicationRecord
   # name     :string, null:false
 
   validates_presence_of :name
-  validates :name, length: { maximum: 10, message: "hashtag can be allowed at most 10 words", }
-  
+  validates :name, length: { maximum: 10, message: "hashtag can be allowed at most 10 words" }
+  validates :name, format: { with: /\p{Han}|\A[a-zA-Z]+\z/,message: "only allows letters" }
+  validates :name, format: { without: /[ ]|[*]|[_]|[?]/,message: "special char is not allowed" }
+
 
   has_many :q_tagships
   has_many :questions, through: :q_tagships, dependent: :restrict_with_error
