732e1d2f832f79137c8aabf995c4e542b8090c80
xs1523c <xs1523c@gmail.com>
2018-07-30 18:52:28 +0800

    set env variable for rails using github login between local and heroku ver9.0

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,10 +19,10 @@ Devise.setup do |config|
 
   if Rails.env.development?
   #local developmenmt時候用這個
-  callback_url: "http://localhost:3000/users/auth/github/callback"
+  callback_url = "http://localhost:3000/users/auth/github/callback"
   elsif Rails.env.production?
   #heroku production時候用這個
-  callback_url: "https://alphacamp-stackoverflow.herokuapp.com/users/auth/github/callback"
+  callback_url = "https://alphacamp-stackoverflow.herokuapp.com/users/auth/github/callback"
   end 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
